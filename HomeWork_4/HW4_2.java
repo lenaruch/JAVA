e8d4bc1672e9e3c80a284f3fdaa63c6f11e24c02
lenaruch <89196825388@mail.ru>
2023-07-10 00:14:32 +0300

    HW4_2 done

--- HomeWork_4/HW4_2.java
+++ HomeWork_4/HW4_2.java
@@ -1,9 +1,73 @@
 package HomeWork_4;
+
+import java.util.*;
+
 /*
     Реализуйте очередь с помощью LinkedList со следующими методами:
     enqueue() - помещает элемент в конец очереди,
-    dequeue() - возвращает первый элемент из очереди и удаляет его,
+    dequeue() - возвращает первый элемент из очереди и удаляет его
     first() - возвращает первый элемент из очереди, не удаляя.
  */
 public class HW4_2 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        // Выводим созданную очередь
+        System.out.println(queue);
+
+        // Добавляем елемент в конец очереди
+        enqueue(queue);
+        System.out.println(queue);
+
+        // Добавляем еще один елемент в конец очереди
+        enqueue(queue);
+        System.out.println(queue);
+
+        // Возвращаем первый элемент из очереди иудаляем его
+        dequeue(queue);
+        System.out.println(queue);
+
+        // Добавляем еще один елемент в конец очереди
+        enqueue(queue);
+        System.out.println(queue);
+
+        // Возвращаем первый элемент из очереди, не удаляя его.
+        first(queue);
+        System.out.println(queue);
+
+    }
+
+    /**
+     * enqueue() - помещает элемент в конец очереди...
+     * Сам элемент генерируется исходя из последнего значения из очереди ( + 1)
+     */
+    static void enqueue (Queue queue){
+        //queue.add(Integer.valueOf(queue.size()+1));
+        Iterator iterator = queue.iterator();
+        int a = 0;
+        while (iterator.hasNext()){
+            a = (int) iterator.next();
+        }
+        queue.add(a+1);
+    }
+
+    /**
+     * dequeue() - возвращает первый элемент из очереди и удаляет его.
+     */
+    static void dequeue(Queue queue){
+        System.out.println(queue.remove());
+    }
+
+    /**
+     * first() - возвращает первый элемент из очереди, не удаляя его.
+     */
+    static void first(Queue queue){
+        System.out.println(queue.peek());
+    }
+
 }
