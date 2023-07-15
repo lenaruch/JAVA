16fbd638b65ebf29ef3b1212c0e0543dabb4c8a5
lenaruch <89196825388@mail.ru>
2023-07-15 16:51:18 +0300

    HW6 сделано на половину

--- HomeWork_6/HW6_1.java
+++ HomeWork_6/HW6_1.java
@@ -1,7 +1,7 @@
 package HomeWork_6;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.security.Key;
+import java.util.*;
 
 /*
     Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
@@ -29,30 +29,37 @@ import java.util.Set;
  */
 public class HW6_1 {
     public static void main(String[] args) {
-        Set<NoteBook> set = new HashSet<>();
+        Set<NoteBook> noteBooks = new HashSet<>();
 
-        NoteBook noteBook_1 = new NoteBook("Samsung", "Si7-8-256","i7 8700k", 8,256, "Windows 10", "black");
-        NoteBook noteBook_2 = new NoteBook("Xiaomi", "Xi9-4-120", "i9 9900kf", 4,120, "DOS", "white");
-        NoteBook noteBook_3 = new NoteBook("Apple", "Ai7-32-512", "i7 4790k", 32,512, "Ubuntu", "silver");
-        NoteBook noteBook_4 = new NoteBook("Huawei", "Hi5-16-1024", "i5 10400f", 16, 1024, "Windows XP", "red");
-        NoteBook noteBook_5 = new NoteBook("Acer", "Ai3-16-256", "i3 1100", 16,256, "Windows 7", "silver");
-        NoteBook noteBook_6 = new NoteBook("Acer", "Ar56-4-512", "AMD Ryzen 5600G", 4,512, "CentOS", "orange");
-        NoteBook noteBook_7 = new NoteBook("Apple", "Ar22-8-1024", "AMD Ryzen 2200U", 8,1024, "Windows", "purple");
-        NoteBook noteBook_8 = new NoteBook("Dexp", "", "Baikal", 64,2048, "Linux", "gray");
-        NoteBook noteBook_9 = new NoteBook("Lenovo", "", "Эльбрус", 128,2048, "DOS", "invisible");
+        NoteBook noteBook_1 = new NoteBook("MSI","Intel","i3", 64,256, "Windows", "black");
+        noteBooks.add(noteBook_1);
+        NoteBook noteBook_2 = new NoteBook("ASUS","Intel","i9", 4,512, "Ubuntu", "gray");
+        noteBooks.add(noteBook_2);
+        NoteBook noteBook_3 = new NoteBook("Apple","Intel","i7", 16,256, "Ubuntu", "silwer");
+        noteBooks.add(noteBook_3);
+        NoteBook noteBook_4 = new NoteBook("Huawei","Intel","i3", 8,2048, "DOS", "white");
+        noteBooks.add(noteBook_4);
+        NoteBook noteBook_5 = new NoteBook("HP","Intel","i5", 32,256, "DOS", "red");
+        noteBooks.add(noteBook_5);
+        NoteBook noteBook_6 = new NoteBook("Acer","Intel","i5", 8,512, "Windows", "white");
+        noteBooks.add(noteBook_6);
+        NoteBook noteBook_7 = new NoteBook("MSI","Intel","i7", 16,1024, "Windows", "white");
+        noteBooks.add(noteBook_7);
 
-        set.add(noteBook_1);
-        set.add(noteBook_2);
-        set.add(noteBook_3);
-        set.add(noteBook_4);
-        set.add(noteBook_5);
-        set.add(noteBook_6);
-        set.add(noteBook_7);
-        set.add(noteBook_8);
-        set.add(noteBook_9);
+        Map<Integer, String> map_prop = new HashMap<>();
+        map_prop.put(1, null); // RAM
+        map_prop.put(2, null); // MEMORY
 
-        System.out.println(noteBook_1.toString());
+        System.out.println("Введите минимальное значение ОЗУ:");
+        Scanner scanner = new Scanner(System.in);
+        String ram = scanner.nextLine();
+        map_prop.put(1, ram);
 
+        System.out.println("Введите минимальный объем накопителя:");
+        String ssd = scanner.nextLine();
+        map_prop.put(2, ssd);
 
+        System.out.println(noteBook_1.getInfo());
     }
 }
+
--- HomeWork_6/NoteBook.java
+++ HomeWork_6/NoteBook.java
@@ -1,25 +1,56 @@
 package HomeWork_6;
 
 public class NoteBook {
-    String vendor;
-    String model;
-    String cpu;
+    String manufacturer;
+    String cpuVendor;
+    String cpu_model;
     int ram;
     int memory;
     String os;
     String color;
 
-    @Override
-    public String toString() {
-        return;
-    }
-
-    public NoteBook(String vendor,String model, String cpu, int ram, int memory, String os, String color) {
-        this.vendor = vendor;
-        this.cpu = cpu;
+    public NoteBook(String manufacturer, String cpuVendor, String cpu_model, int ram, int memory, String os, String color) {
+        this.manufacturer = manufacturer;
+        this.cpuVendor = cpuVendor;
+        this.cpu_model = cpu_model;
         this.ram = ram;
         this.memory = memory;
         this.os = os;
         this.color = color;
+    }
+
+
+    public String getInfo() {
+        String res = String.format("Прозводитель: %s\nCPU: %s %s\nRAM: %d\nSSD: %d",
+                manufacturer, cpuVendor, cpu_model,ram, memory);
+        return res;
+    }
+
+    public String getManufacturer(){
+        return manufacturer;
+    }
+
+    public String getCpuVendor(){
+        return cpuVendor;
+    }
+
+    public String getCpu_model(){
+        return cpu_model;
+    }
+
+    public int getRam(){
+        return ram;
+    }
+
+    public int getMemory(){
+        return memory;
+    }
+
+    public String getOs(){
+        return os;
+    }
+
+    public String getColor(){
+        return color;
     }
 }
